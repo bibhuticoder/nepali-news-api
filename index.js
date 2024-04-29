e1c44ebd86aac3c61bc433b755f4edb919e6bb31
bibhuti <bibhutipd@gmail.com>
2024-04-29 14:49:37 +0545

    test 4

--- index.js
+++ index.js
@@ -12,6 +12,6 @@ const fs = require("fs");
     // console.log("====================KP=====================")
     // await kpScraper.scrape()
 
-    fs.writeFileSync("./__daily_news/test.txt", "test 3");
+    fs.writeFileSync("./__daily_news/test.txt", "test 4");
     console.log("File added");
 })()
