bf30cf98c66ccdfcf53af834811b90d26ca67114
bibhuti <bibhutipd@gmail.com>
2024-04-29 15:48:58 +0545

    test scraping

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
 const okScraper = require("./scrapers/ok.scraper");
 const nnScraper = require("./scrapers/nn.scraper");
 const kpScraper = require("./scrapers/kp.scraper");
-const fs = require("fs");
+// const fs = require("fs");
+
 (async () => {
     // console.log("====================OK=====================")
-    // await okScraper.scrape()
+    await okScraper.scrape()
 
     // console.log("====================NN=====================")
-    // await nnScraper.scrape()
+    await nnScraper.scrape()
 
     // console.log("====================KP=====================")
-    // await kpScraper.scrape()
+    await kpScraper.scrape()
 
-    fs.writeFileSync("./__daily_news/test.txt", "test 19");
-    console.log("File added");
+    // For test
+    // fs.writeFileSync("./__daily_news/test.txt", "test 19"); 
 })()
